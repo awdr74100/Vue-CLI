36427e219f6a7df8e685ce9a96ecc258df6bdb46
藍奕濡 <a78945612385238@gmail.com>
2019-09-13 12:23:59 +0800

    20190913

--- pet-shopping/src/components/userSidebar.vue
+++ pet-shopping/src/components/userSidebar.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="userSidebar">
-      <div class="logo"></div>
+      <div class="logo"><router-link to="/"></router-link></div>
       <input type="text" name="search" id="" placeholder="搜尋商品">
-      <ul class="list">
+      <ul class="list" v-if="visibility == 'b'">
         <li class="list__item">
           <router-link to="/">首　　頁</router-link>
         </li>
@@ -12,15 +12,38 @@
         </li>
         <li class="list__item">
           <router-link to="/ProductList">嚴選商品</router-link>
+          <span class="arrow arrow--Right"><i class="fas fa-chevron-right"></i></span>
         </li>
         <li class="list__item"><a href="#">最新消息</a></li>
         <li class="list__item"><a href="#">領養專區</a></li>
         <li class="list__item"><a href="#">聯絡我們</a></li>
-        <li class="list__item mt-30">
-          <router-link to="/admin/productList"><i class="fas fa-user-cog"></i>後台管理</router-link>
-        </li>
-        <li class="list__item"><a href="#"><i class="fas fa-shopping-cart"></i>購物車</a></li>
       </ul>
+      <!-- <ul class="list">
+        <li class="list__item" @click="visibility = 'b' ">
+          <span class="arrow arrow--Left"><i class="fas fa-chevron-left"></i></span>
+          <router-link to="/ProductList">貓咪飼料</router-link>
+        </li>
+        <li class="list__item">
+          <router-link to="/ProductList">貓咪罐頭</router-link>
+        </li>
+        <li class="list__item">
+          <router-link to="/ProductList">貓咪用品</router-link>
+        </li>
+        <li class="list__item">
+          <router-link to="/ProductList">狗狗飼料</router-link>
+        </li>
+        <li class="list__item">
+          <router-link to="/ProductList">狗狗罐頭</router-link>
+        </li>
+        <li class="list__item">
+          <router-link to="/ProductList">狗狗零食</router-link>
+        </li>
+      </ul> -->
+      <div class="user">
+        <router-link to="/admin/productList"><span class="userIcon"><i class="fas fa-user-cog"></i></span>後台管理
+        </router-link>
+        <a href="#"><span class="userIcon"><i class="fas fa-shopping-cart"></i></span>購物車</a>
+      </div>
       <div class="privacy">
         <h4>隱私權政策</h4>
         <p>Copyright © 2019 by Roya</p>
@@ -29,3 +52,14 @@
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        visibility: 'b',
+      }
+    },
+  }
+
+</script>
