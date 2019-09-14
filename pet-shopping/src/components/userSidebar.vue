349762e630c0ea881d23859d2fa7207c10fe034f
藍奕濡 <a78945612385238@gmail.com>
2019-09-15 01:46:16 +0800

    20190915

--- pet-shopping/src/components/userSidebar.vue
+++ pet-shopping/src/components/userSidebar.vue
@@ -1,6 +1,43 @@
 <template>
   <div>
     <div class="userSidebar">
+      <div class="logo"><a href="#"></a></div>
+      <input type="text" name="search" placeholder="搜尋商品">
+      <ul class="list">
+        <li class="list__item">
+          <a href="#">首　　頁</a>
+        </li>
+        <li class="list__item">
+          <a href="#">關於我們</a>
+        </li>
+        <li class="list__item">
+          <a href="#" @click="visibility = !visibility">嚴選商品</a>
+          <ul class="dropdownList" :class="{'dropdownList--active':visibility}">
+            <li><a href="#">貓咪用品</a></li>
+            <li><a href="#">狗狗用品</a></li>
+            <li><a href="#">貓咪飼料</a></li>
+            <li><a href="#">狗狗飼料</a></li>
+          </ul>
+        </li>
+        <li class="list__item">
+          <a href="#">最新消息</a>
+        </li>
+        <li class="list__item">
+          <a href="#">領養專區</a>
+        </li>
+        <li class="list__item">
+          <a href="#">聯絡我們</a>
+        </li>
+      </ul>
+      <div class="privacy">
+        <h4>隱私權政策</h4>
+        <p>Copyright © 2019 by Roya</p>
+        <span><i class="fab fa-github"></i><i class="fab fa-codepen ml-20"></i></span>
+      </div>
+    </div>
+
+
+    <!-- <div class="userSidebar">
       <div class="logo"><router-link to="/"></router-link></div>
       <input type="text" name="search" id="" placeholder="搜尋商品">
       <ul class="list" v-if="visibility == 'b'">
@@ -18,27 +55,6 @@
         <li class="list__item"><a href="#">領養專區</a></li>
         <li class="list__item"><a href="#">聯絡我們</a></li>
       </ul>
-      <!-- <ul class="list">
-        <li class="list__item" @click="visibility = 'b' ">
-          <span class="arrow arrow--Left"><i class="fas fa-chevron-left"></i></span>
-          <router-link to="/ProductList">貓咪飼料</router-link>
-        </li>
-        <li class="list__item">
-          <router-link to="/ProductList">貓咪罐頭</router-link>
-        </li>
-        <li class="list__item">
-          <router-link to="/ProductList">貓咪用品</router-link>
-        </li>
-        <li class="list__item">
-          <router-link to="/ProductList">狗狗飼料</router-link>
-        </li>
-        <li class="list__item">
-          <router-link to="/ProductList">狗狗罐頭</router-link>
-        </li>
-        <li class="list__item">
-          <router-link to="/ProductList">狗狗零食</router-link>
-        </li>
-      </ul> -->
       <div class="user">
         <router-link to="/admin/productList"><span class="userIcon"><i class="fas fa-user-cog"></i></span>後台管理
         </router-link>
@@ -49,7 +65,7 @@
         <p>Copyright © 2019 by Roya</p>
         <span><i class="fab fa-github"></i><i class="fab fa-codepen ml-20"></i></span>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -57,7 +73,7 @@
   export default {
     data() {
       return {
-        visibility: 'b',
+        visibility: false,
       }
     },
   }
