4006ce818ac924d0d7a4699a42894bd168151e36
藍奕濡 <a78945612385238@gmail.com>
2019-09-25 00:23:29 +0800

    20190925

--- pet-shopping/src/components/CheckoutStep.vue
+++ pet-shopping/src/components/CheckoutStep.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul class="checkoutStep">
-      <li class="checkoutStep__item">
+      <!-- <li class="checkoutStep__item">
         <div class="stepSection">
           <span class="step step--active">1</span>
           <p class="name name--active">購物車清單</p>
@@ -24,6 +24,22 @@
           <span class="step">4</span>
           <p class="name">送出訂單</p>
         </div>
+      </li> -->
+      <li class="checkoutStep__item checkoutStep--resActive">
+        <span class="step">1</span>
+        <p class="name">購物車清單</p>
+      </li>
+      <li class="checkoutStep__item checkoutStep--active">
+        <span class="step">1</span>
+        <p class="name">收件人資料</p>
+      </li>
+      <li class="checkoutStep__item">
+        <span class="step">1</span>
+        <p class="name">購買明細確認</p>
+      </li>
+      <li class="checkoutStep__item checkoutStep--cancel">
+        <span class="step">1</span>
+        <p class="name">送出訂單</p>
       </li>
     </ul>
   </div>
