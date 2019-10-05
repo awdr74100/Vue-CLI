96fd92b4ac6f42cc83ce30819f89f92053dac8f9
藍奕濡 <a78945612385238@gmail.com>
2019-10-06 00:53:23 +0800

    20191006

--- pet-shopping/src/components/userContact.vue
+++ pet-shopping/src/components/userContact.vue
@@ -10,16 +10,24 @@
           <div class="row">
             <div class="col-12">
               <div class="location">
+                <div class="company">
+                  <h2>聯絡我們</h2>
+                  <p class="address">地址：<span>台北市南港區三重路19之6號7樓(南港軟體園區C棟)</span></p>
+                  <p>電話：<span>02-0000-0000</span></p>
+                  <p>傳真：<span>02-0000-0000</span></p>
+                  <p>手機：<span>0912-345678</span></p>
+                  <p>郵箱：<span>service@gmail.com</span></p>
+                </div>
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1807.1613726459457!2d121.6148286!3d25.0570479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ab5881ed5a01%3A0xf1fd9515ad913fc4!2zMTE15Y-w5YyX5biC5Y2X5riv5Y2A5LiJ6YeN6LevMTktNuiZnw!5e0!3m2!1szh-TW!2stw!4v1570272206483!5m2!1szh-TW!2stw"
-                  width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                  width="70%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col-12">
               <div class="message">
-                <div class="name">問題反應<i class="fas fa-comment-dots"></i></div>
+                <div class="name">意見回饋<i class="far fa-comment-alt"></i></div>
                 <p class="ex">如果您有任何疑問，或想回饋反映的意見，歡迎與我們聯繫。</p>
                 <form action="#" class="form">
                   <div class="inputRow">
